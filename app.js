29360b90b93a4c985657e355b0a7cc97a688ea0e
mlouis13 <mlouis@edenschool.fr>
2026-03-16 17:30:23 +0100

    ptit repo

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const code = document.querySelector("#menu-code-status");
 const body = document.querySelector("body");
 const h1 = document.querySelector("h1");
 const img = document.querySelector("img");
+const color = document.querySelector("#color");
 let isClicked = false;
+body.classList.add("idlol");
 light.addEventListener("click", () => {
 	dialog.showModal();
 });
@@ -14,15 +16,16 @@ form.addEventListener("submit", () => {
 	if (isClicked == false) {
 		span.textContent = "OFF";
 		code.textContent = "OFF";
-		body.classList.add("light-on");
-		body.classList.remove("light-off");
+		body.style.backgroundColor =
+			"rgba(255, 222, 173, " + color.value / 100 + ")";
+		console.log(color.value);
 		img.setAttribute("src", "assets/glowstone-on.webp");
 		isClicked = true;
 	} else if (isClicked == true) {
 		span.textContent = "ON";
 		code.textContent = "ON";
-		body.classList.add("light-off");
-		body.classList.remove("light-on");
+		console.log(color.value);
+		body.style.backgroundColor = "rgba(34, 25, 13, " + color.value / 100 + ")";
 		img.setAttribute("src", "assets/glowstone-off.webp");
 		isClicked = false;
 	}
